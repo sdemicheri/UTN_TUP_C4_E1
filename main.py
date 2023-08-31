234ef199210a648261cdfc8c3ff43da45853e87f
Gaston Godoy Dechecco <gaston.vm2000@hotmail.com>
2023-08-31 14:48:23 -0300

    primer commit

--- main.py
+++ main.py
@@ -1,3 +1,35 @@
+print("-----------------------------Ejercicio 1-----------------------------")
+planilla_de_corredores = """
+======================
+Corredores / Puesto
+Brian         1ro
+Carlos        2do
+Diego         3ro
+Andres        4to
+======================
+"""
+print(planilla_de_corredores)
+viaje = """
+                Viaje a la playa
+Parejas:  Axel/Lucia  Dario/Andrea  Tomas/Marlene        
+Transporte:  tren        avion          coche
+"""
+print (viaje)
+print ("-----------------------------Ejercicio 2-----------------------------")
+nombre = input ("Nombre del cumpleañero: ")
+tarjeta_feliz_cumpleaños = """
+▐█▀▐█▀▌▐▌█▀▐██▌█▐▀▄▐█▀
+▐█▀▐██▄▐▌█▀▐█▐▌█▐░█░▀█
+▐█░▐█░█▐▌██▐█▐██▐█▀▐██
+░░░░░░Feliz Cumpleaños░░░░░░░░"""
+print (tarjeta_feliz_cumpleaños)   
+print ("✨✨✨✨✨ ",nombre,"✨✨✨✨✨")   
+# Profe, en la guia de ejercicios de esta entrega cuando la descargo
+# me sale la pagina de los vasos que hicimos la ultima clase para hacer,
+# pero como ya lo hicimos en clase y no hay consigna, nose si sera esto
+# lo que se pide y esta mal la pagina. Hice la tarjeta de feliz cumpleaños que ya
+# habia entregado, pero esta vez agregando el "input" que tambien lo vimos la ultima clase.
+print ("-----------------------------Ejercicio 3-----------------------------")
 print ("*********************")
 print ("⚽⚽⚽⚽⚽⚽⚽⚽⚽⚽")
 nombre_del_arbitro_1 = input ("Arbitro 1: ")
@@ -10,6 +42,7 @@ fecha = input ("Fecha: ")
 horario = input ("Horario: ")
 print ("*********************")
 # abajo lo mismo, pero con funcion def:
+print ("lo mismo pero con funcion def ↓")
 def asterisco():
     print ("*********************")
 def emoticonfutbol():
@@ -33,8 +66,47 @@ Cancha()
 Fecha()
 Horario()
 asterisco()
+print ("-----------------------------Ejercicio 4-----------------------------")
+print ("𝐈𝐧𝐠𝐫𝐞𝐬𝐞 𝐝𝐚𝐭𝐨𝐬 𝐝𝐞𝐥 𝐩𝐚𝐜𝐢𝐞𝐧𝐭𝐞")
+nombre = input ("Nombre: ")
+apellido = input ("Apellido: ")
+nacimiento = input ("Fecha de nacimiento: ")
+dni = input ("Dni: ")
+print ("𝐈𝐧𝐠𝐫𝐞𝐬𝐞 𝐝𝐚𝐭𝐨𝐬 𝐝𝐞𝐥 𝐦𝐞𝐝𝐢𝐜𝐚𝐦𝐞𝐧𝐭𝐨")
+medicamento = input ("Medicamento Recetado: ")
+dosis = input ("Dosis del medicamento: ")
+instrucciones = input ("Instrucciones del medicamento: ")
+print ("𝐈𝐧𝐠𝐫𝐞𝐬𝐞 𝐝𝐚𝐭𝐨𝐬 𝐞𝐱𝐭𝐫𝐚𝐬")
+doctor_doctora= input ("Recetado por doctor/doctora (nombre de quien lo receta): ")
+Fecha = input ("Fecha de emision: ")
+turno = input ("Fecha del proximo turno: ")
+print (""" _________________________________________
+|           𝑪𝒐𝒏𝒔𝒖𝒍𝒕𝒐𝒓𝒊𝒐 𝒎𝒆𝒅𝒊𝒄𝒐            |
+|                𝕮𝖗𝖎𝖘𝖙𝖔𝖇𝖆𝖑                |
+|Direccion: Av. inventada 1234            |
+|_________________________________________| """)
+print ("           D𝐚𝐭𝐨𝐬 𝐝𝐞𝐥 𝐩𝐚𝐜𝐢𝐞𝐧𝐭𝐞")
+print ("Nombre:",nombre)
+print ("Apellido:",apellido)
+print ("Fecha de nacimiento:", nacimiento)
+print ("Dni:",dni)
+print ("""
+               𝐑𝐞𝐬𝐞𝐭𝐚       """)
+print ("Medicamento:",medicamento)
+print ("Dosis:", dosis)
+print ("Instrucciones:",instrucciones)
+print ("""     
+               𝐅𝐞𝐜𝐡𝐚𝐬""")
+print ("Fecha de emision:",Fecha)
+print ("Proximo turno:",turno)
+print ("""
+       """)
+print ("             ","Doctor/Doctora:",doctor_doctora)
+print ("               ","Firma/Sello")
 
 
+
+                   
 
 
 
