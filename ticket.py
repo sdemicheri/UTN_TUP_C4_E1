5b37c17ddf43e74bff6f52f441aeb7e952ad2017
AgustinnCAI <britosagus15@gmail.com>
2023-09-05 00:43:25 -0300

    ticket agustin britos

---
+++ ticket.py
@@ -0,0 +1,24 @@
+
+cancha = input("Ingrese el nombre de la cancha: ")
+fecha = input("Ingrese la fecha del partido: ")
+hora = input("Ingrese la hora del partido: ")
+
+arbitro1 = input("Ingrese el nombre del Árbitro 1: ")
+arbitro2 = input("Ingrese el nombre del Árbitro 2: ")
+arbitro3 = input("Ingrese el nombre del Árbitro 3: ")
+arbitro4 = input("Ingrese el nombre del Árbitro 4: ")
+
+print("Partido de fútbol")
+print("Cancha:", cancha)
+print("Fecha:", fecha)
+print("Hora:", hora)
+print("Árbitros:")
+print("Árbitro 1:", arbitro1)
+print("Árbitro 2:", arbitro2)
+print("Árbitro 3:", arbitro3)
+print("Árbitro 4:", arbitro4)
+
+print("El lugar del partido será en " + cancha)
+print("El horario del partido será a las " + hora)
+print("Se disputará en la fecha " + fecha)
+print("Los árbitros serán: " + arbitro1 + ", " + arbitro2 + ", " + arbitro3 + ", " + arbitro4)
