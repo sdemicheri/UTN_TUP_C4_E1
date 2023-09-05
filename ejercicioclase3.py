212626c50304694473c36abdd6b840236005ccc8
zernottiemilio <zernottiemilio@gmail.com>
2023-09-05 15:38:19 -0300

    segundo archivo

---
+++ ejercicioclase3.py
@@ -0,0 +1,18 @@
+print("-----------ejecricio clase 3-----------\n")
+
+valor_x_hora = 1500
+horasTrabajadas = float(input("ingrese la cantidad de horas que trabajo:"))
+print("----------------------------------------------\n")
+pagoTotal = horasTrabajadas * valor_x_hora
+
+print(f"el pago total es de: ${pagoTotal}")
+
+
+
+
+
+
+
+
+
+
