c0fafb2b046e283e0d7ef51891f78dfddeafbb3b
joaquinStuppa <joaquinstuppa1@gmail.com>
2023-09-05 12:15:05 -0300

    entregaTicket

---
+++ main.py
@@ -0,0 +1,13 @@
+
+nombre = input("ingrese el nombre completo del paciente: ")
+nacimiento = input("ingrese la fecha de nacimiento del paciente: ")
+direccion = input("ingrese la direccion de la clinica: ")
+detalles = input("ingrese el nombre del medicamento, la dosis y las instrucciones de uso: ")
+
+print("nombre:", nombre)
+print("nacimiento:", nacimiento)
+print("direccion:", direccion)
+print("detalles:", detalles)
+
+
+
