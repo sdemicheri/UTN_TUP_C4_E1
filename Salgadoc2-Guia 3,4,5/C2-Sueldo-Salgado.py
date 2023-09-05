4f3a682c21e02200d5e6a349b6a7e2b8dcd18ac0
Carlos <128559215+carlossalgado1@users.noreply.github.com>
2023-09-04 22:28:20 -0300

    Delete Salgadoc2-Guia 3,4,5 directory

--- Salgadoc2-Guia 3,4,5/C2-Sueldo-Salgado.py
+++
@@ -1,8 +0,0 @@
-#Pedir informacion
-horas_trabajo = input("⌚Ingrese cuantas horas de trabajo realizo este mes⌚ : ")
-costo_hora = input("💵Ingrese cual es el costo por hora de trabajo💵 : ")
-
-#Imprimir informacion, realizar calculo, mensaje con el sueldo
-
-sueldo = int(horas_trabajo)*int(costo_hora)
-print(f"Su sueldo de este mes es: ${sueldo}")
--- Salgadoc2-Guia 3,4,5/Guia5Eje2.py
+++
@@ -1,8 +0,0 @@
-#Ingrese los coeficientes en una funcion cuadratica y le calcula las raices, si es que estan en los reales
-a, b = float(input("Ingrese el coeficiente de X^2\n")), float(input("Ingrese el coeficiente de X\n"))
-c = float(input("Ingrese el termino independiente\n"))
-if b**2-4*a*c >= 0:
-    x1, x2 = (-b+(b**2-4*a*c)**(1/2))/(2*a), (-b-(b**2-4*a*c)**(1/2))/(2*a)
-    print(f"\nLas raices son {x1} y {x2}")
-else:
-    print("\nNo existen raices dentro de los numeros reales con los parametros ingresados")
