c897c4d7198db3530d29f6600d40c506ab89a6ec
joaquinStuppa <joaquinstuppa1@gmail.com>
2023-09-05 11:54:27 -0300

    holamundo

---
+++ stuppaticket.py.py
@@ -0,0 +1,23 @@
+
+
+cancha = input("ingrese el nombre de la cancha: ")
+fecha = input("ingrese la fecha del partido: ")
+hora = input("ingrese la hora del partido: ")
+
+arbitro1 = input("ingrese el nombre del arbitro 1: ")
+arbitro2 = input("ingrese el nombre del arbitro 2: ")
+arbitro3 = input("ingrese el nombre del arbitro 3: ")
+arbitro4 = input("ingrese el nombre del arbitro 4: ")
+
+print("partido de futbol")
+print("cancha:", cancha)
+print("fecha:", fecha)
+print("hora:", hora)
+print("arbitros:")
+print("arbitro 1:", arbitro1)
+print("arbitro 2:", arbitro2)
+print("arbitro 3:", arbitro3)
+print("arbitro 4:", arbitro4)
+
+
+
