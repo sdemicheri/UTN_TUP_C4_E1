3d343e0463af2a250716e643aa844ced0f6f86c1
zernottiemilio <zernottiemilio@gmail.com>
2023-09-04 23:49:24 -0300

    recetamedica.py

---
+++ recetamedica.py
@@ -0,0 +1,32 @@
+print("-------- laboratorio de computación guia n3--------")
+
+print("clinica san martin")
+print("dr: marcelo caneparo🥼")
+print("medico gastronterologo")
+print("dirección: jujuy 1072")
+print("---------------------------------------------")
+paciente = (input("ingrese su nombre y apellido: "))
+fecha_de_nacimiento = (input("ingrese su fecha de nacimiento: "))
+medicamento = (input("ingrese el medicamento recetado: "))
+dosis = (input("ingrese dosis diaria de uso: "))
+instrucciones = (input("ingrese la forma de uso: "))
+prox_consulta = (input("ingrese el fecha y hora de la proxima consulta: "))
+
+print("---------------------------------------------")
+
+print(paciente)
+print(fecha_de_nacimiento)
+print(medicamento)
+print(dosis)
+print(instrucciones)
+print("---------------------------------------------")
+print(prox_consulta)
+print("---------------------------------------------")
+
+
+
+
+
+
+
+
