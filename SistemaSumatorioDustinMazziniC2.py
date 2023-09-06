58a51839ee33a22b2d7cf17cae272641376d53d1
DustinUTNVM <142992319+DustinUTNVM@users.noreply.github.com>
2023-09-05 22:08:01 -0300

    Add files via upload

---
+++ SistemaSumatorioDustinMazziniC2.py
@@ -0,0 +1,18 @@
+def suma_numeros():
+    n1 = int(input("Ingrese el n1"))
+    n2 = int(input("ingrese el n2"))
+    n3 = int(input("ingrese el n3"))
+    n4 = int(input("ingrese el n4"))
+
+    suma = n1 + n2 + n3 + n4
+    return suma
+def suma_numeros():
+    n1 = int(input("Ingrese el n1"))
+    n2 = int(input("ingrese el n2"))
+    n3 = int(input("ingrese el n3"))
+    n4 = int(input("ingrese el n4"))
+
+    suma = n1 + n2 + n3 + n4
+    return suma
+
+print(suma_numeros())
