35f5387cff221f6c2ac3a784d4902874cc4463d0
DustinUTNVM <142992319+DustinUTNVM@users.noreply.github.com>
2023-09-05 21:54:54 -0300

    Add files via upload

---
+++ TrabajodeFutbolDustinMazziniC2.py
@@ -0,0 +1,29 @@
+def imprimirArbitros(a1, a2, a3, a4):
+    print('🏆🏆ARBITROS DESIGNADOS🏆🏆')
+    print(a1)
+    print(a2)
+    print(a3)
+    print(a4)
+    print("⚽⚽⚽⚽⚽⚽⚽⚽⚽⚽⚽⚽")
+
+    def imprimirDatosCancha(nombre, dia):
+        print("LUGAR")
+        print(nombre)
+        print("FECHA")
+        print(dia)
+
+
+a1 = input('Ingrese el nombre del arbitro a1: ')
+a2 = input('Ingrese el nombre del arbitro a2: ')
+a3 = input('Ingrese el nombre del arbitro a3: ')
+a4 = input('Ingrese el nombre del arbitro a4: ')
+cancha = input('Ingrese el nombre de la cancha: ')
+dia = input('Ingrese el dia: ')
+imprimirArbitros(a1, a2, a3, a4)
+
+
+def imprimirDatosCancha(cancha, dia):
+    pass
+
+
+imprimirDatosCancha(cancha, dia)
